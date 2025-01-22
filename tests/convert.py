877b3bfe27ab6a73137e96ba35bdc64cba51e448
whimsee <jbtenmatay@gmail.com>
2025-01-22 15:45:40 -0800

    testing

--- tests/convert.py
+++ tests/convert.py
@@ -1,5 +1,6 @@
 import sqlite3
 import json
+import hashlib
 con = sqlite3.connect("tutorial.db")
 cur = con.cursor()
 # # cur.execute("CREATE TABLE movie(title, year, score)")
@@ -10,15 +11,15 @@ res = cur.execute("SELECT key FROM keys")
 
 keys_raw = res.fetchone()[0]
 print(keys_raw)
-keys_strip = keys_raw.strip("[]")
-keys_split = keys_strip.split(",")
+keys_split = keys_raw.split(",")
 
 for num in keys_split:
     print(index[int(num)])
     item = int(index[int(num)])
     print("SELECT {} FROM {} WHERE id = {}".format("value", "val", item))
     res = cur.execute("SELECT {} FROM {} WHERE id = {}".format("value", "vals", item))
-    print(cur.fetchall())
+    value = cur.fetchone()[0]
+    print(value)
 
 con.close()
 
