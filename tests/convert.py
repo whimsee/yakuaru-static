b86a7b29f87c2d10fd27f613ba46a8bdcb504132
whimsee <jbtenmatay@gmail.com>
2025-01-22 13:12:49 -0800

    testing

---
+++ tests/convert.py
@@ -0,0 +1,8 @@
+import sqlite3
+con = sqlite3.connect("tutorial.db")
+cur = con.cursor()
+# cur.execute("CREATE TABLE movie(title, year, score)")
+
+res = cur.execute("SELECT name FROM sqlite_master")
+print(res.fetchone())
+con.close()
