a5ba908ca2ca4a1d616639f09fbe7a177b6df163
whimsee <jbtenmatay@gmail.com>
2025-01-22 15:25:19 -0800

    testing

--- tests/convert.py
+++ tests/convert.py
@@ -4,6 +4,8 @@ con = sqlite3.connect("tutorial.db")
 cur = con.cursor()
 # # cur.execute("CREATE TABLE movie(title, year, score)")
 
+index = [2,6,8,1,7,3]
+
 res = cur.execute("SELECT key FROM keys")
 
 keys_raw = res.fetchone()[0]
@@ -12,7 +14,13 @@ keys_strip = keys_raw.strip("[]")
 keys_split = keys_strip.split(",")
 
 for num in keys_split:
-    print(num)
+    print(index[int(num)])
+    item = int(index[int(num)])
+    print("SELECT {} FROM {} WHERE id = {}".format("value", "val", item))
+    res = cur.execute("SELECT {} FROM {} WHERE id = {}".format("value", "vals", item))
+    print(cur.fetchall())
+
+con.close()
 
 # with open('glossaryMaster.json') as file:
 #     data = json.load(file)
