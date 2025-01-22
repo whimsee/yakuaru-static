6cb2378145e85b85e4728578cb5a7b6389edd625
whimsee <jbtenmatay@gmail.com>
2025-01-22 13:52:25 -0800

    testing

--- tests/convert.py
+++ tests/convert.py
@@ -1,8 +1,16 @@
 import sqlite3
-con = sqlite3.connect("tutorial.db")
-cur = con.cursor()
-# cur.execute("CREATE TABLE movie(title, year, score)")
+import json
+# con = sqlite3.connect("tutorial.db")
+# cur = con.cursor()
+# # cur.execute("CREATE TABLE movie(title, year, score)")
 
-res = cur.execute("SELECT name FROM sqlite_master")
-print(res.fetchone())
-con.close()
+# res = cur.execute("SELECT name FROM sqlite_master")
+# print(res.fetchone())
+# con.close()
+
+with open('glossaryMaster.json') as file:
+    data = json.load(file)
+
+for items in data:
+    # print(data[items])
+    print(data[items]["tl"])
