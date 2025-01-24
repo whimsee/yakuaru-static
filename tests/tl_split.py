60055ceebba47932a97b7a5406e284e5f798b217
whimsee <jbtenmatay@gmail.com>
2025-01-24 14:08:02 -0800

    testing

--- tests/tl_split.py
+++ tests/tl_split.py
@@ -8,6 +8,12 @@ def get_item(data, items, field):
     except KeyError as e:
         return None
 
+def get_tl(data, field):
+    try:
+        return data[field]
+    except KeyError as e:
+        return None
+
 with open('glossaryMaster.json') as file:
     data = json.load(file)
 
@@ -22,8 +28,39 @@ with open('glossaryMaster.json') as file:
         altsearch = get_item(data, items, "altsearch")
         tl = get_item(data, items, "tl")
         
-        print(tl)
+        # print(tl)
         for entries in tl:
             print(entries)
-        
+            # for x in entries:
+            #     print(get_item(tl, entries, "def"))
+            definition = get_tl(entries, "def")
+            source = get_tl(entries, "src")
+            jpsam = get_tl(entries, "jpsam")
+            ensam = get_tl(entries, "ensam")
+            credit = get_tl(entries, "credit")
+            img = get_tl(entries, "img")
+
+            print(definition)
+            print(source)
+            print(jpsam)
+            print(ensam)
+            print(credit)
+            print(img)
+
+
         break
+
+#  def
+#     defexp
+#     src[]
+#     samples
+#         - id
+#         - jpsam
+#         - ensam
+#     jpsam (refer to samples)
+#     ensam (refer to samples)
+#     credit
+#     genre[](remove)
+#     image 
+#         - id
+#         - link
