a0acb5d84bd6327360eb8c61b7e96a8bc36f8000
whimsee <jbtenmatay@gmail.com>
2025-02-06 15:48:12 -0800

    testing

--- tests/db.py
+++
@@ -1,7 +0,0 @@
-from sqlmodel import SQLModel, create_engine
-from . import models
-
-sqlite_file_name = "yakutest.db"
-sqlite_url = f"sqlite:///{sqlite_file_name}"
-
-engine = create_engine(sqlite_url, echo=True)
