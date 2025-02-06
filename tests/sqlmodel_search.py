ea1cf11b2e88997211b57e0ecff151d6dc4f0eaf
whimsee <jbtenmatay@gmail.com>
2025-02-06 15:43:43 -0800

    testing

--- tests/sqlmodel_search.py
+++ tests/sqlmodel_search.py
@@ -1,5 +1,6 @@
 from models import *
 from sqlmodel import Session, select, col
+from sqlalchemy.orm.exc import NoResultFound
 
 sqlite_file_name = "yakuaru.db"
 sqlite_url = f"sqlite:///{sqlite_file_name}"
@@ -11,7 +12,8 @@ engine = create_engine(sqlite_url)
 with Session(engine) as session:
     # statement = select(Terms)
     # search_term = "["
-    search_term = "仕方"
+    # search_term = "仕方"
+    search_term = "あ"
     # Search LIKE
     statement = select(Terms).where(col(Terms.name).contains(search_term))
     results = session.exec(statement)
@@ -25,3 +27,18 @@ with Session(engine) as session:
             print(terms.name)
             for tls in terms.tl:
                 print(tls.definition)
+
+    search_term = "たしか"
+    # search_term = "仕方"
+
+    statement = select(Terms).where(Terms.name == search_term)
+    results = session.exec(statement)
+    try:
+        one_result = results.one()
+        print(one_result is None)
+        print(one_result.name)
+    except NoResultFound:
+        print("No result")
+    
+    # for terms in one_result:
+    #     print(terms)
