f045b8440caaa5716fd30b143f1cb0fca2164e59
whimsee <jbtenmatay@gmail.com>
2025-02-06 15:51:19 -0800

    testing

--- tests/sqlmodel/sqlmodel_search.py
+++ tests/sqlmodel/sqlmodel_search.py
@@ -9,36 +9,40 @@ sqlite_url = f"sqlite:///{sqlite_file_name}"
 # engine = create_engine(sqlite_url, echo=True)
 engine = create_engine(sqlite_url)
 
-with Session(engine) as session:
-    # statement = select(Terms)
-    # search_term = "["
-    # search_term = "仕方"
-    search_term = "あ"
-    # Search LIKE
-    statement = select(Terms).where(col(Terms.name).contains(search_term))
-    results = session.exec(statement)
-    all_results = results.all()
-    print(len(all_results))
+def search(search_term):
+    with Session(engine) as session:
+        # statement = select(Terms)
+        # search_term = "["
+        # search_term = "仕方"
+        search_term = "あ"
+        # Search LIKE
+        statement = select(Terms).where(col(Terms.name).contains(search_term))
+        results = session.exec(statement)
+        all_results = results.all()
+        print(len(all_results))
 
-    if len(all_results) == 0:
-        print("None")
-    else:
-        for terms in all_results:
-            print(terms.name)
-            for tls in terms.tl:
-                print(tls.definition)
+        if len(all_results) == 0:
+            print("None")
+        else:
+            for terms in all_results:
+                print(terms.name)
+                for tls in terms.tl:
+                    print(tls.definition)
 
-    search_term = "たしか"
-    # search_term = "仕方"
+def search_one(search_term):
+    with Session(engine) as session:
+        statement = select(Terms).where(Terms.name == search_term)
+        results = session.exec(statement)
+        try:
+            one_result = results.one()
+            print(one_result is None)
+            print(one_result.name)
+        except NoResultFound:
+            print("No result")
+        
+        # for terms in one_result:
+        #     print(terms)
 
-    statement = select(Terms).where(Terms.name == search_term)
-    results = session.exec(statement)
-    try:
-        one_result = results.one()
-        print(one_result is None)
-        print(one_result.name)
-    except NoResultFound:
-        print("No result")
     
-    # for terms in one_result:
-    #     print(terms)
+search_one("たしか")
+search_one("仕方")
